8c2670a590358a7b95be77b0b08579d278d81976
Fabrício <fabriciolak12@gmail.com>
2023-04-03 20:53:57 -0300

    feat: understanding fundamental about prisma

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Fabrício',
+    email: 'fabriciolak12@gmail.com',
+  },
+})
 
 app.get('/', async (request, reply) => {
   reply.send('oi')
