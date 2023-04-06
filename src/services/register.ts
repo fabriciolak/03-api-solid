10b4b49b7a0ac2d25ec8bbfcca8bd1d39c971bef
Fabrício <fabriciolak12@gmail.com>
2023-04-06 17:13:28 -0300

    feat: services pattern

---
+++ src/services/register.ts
@@ -0,0 +1,35 @@
+import { prisma } from '@/lib/prisma'
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
+import { hash } from 'bcryptjs'
+
+interface RegisterServiceRequest {
+  name: string
+  email: string
+  password: string
+}
+
+export async function registerService({
+  name,
+  email,
+  password,
+}: RegisterServiceRequest) {
+  const password_hash = await hash(password, 6)
+
+  const emailAlreadyExists = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (emailAlreadyExists) {
+    throw new Error('Email already exists')
+  }
+
+  const prismaUsersRepository = new PrismaUsersRepository()
+
+  await prismaUsersRepository.create({
+    name,
+    email,
+    password_hash,
+  })
+}
