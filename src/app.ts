2c64b2dde0d781bb466b16630dcf17e68731d046
Fabrício <fabriciolak12@gmail.com>
2023-04-05 21:11:01 -0300

    feat: route to create user

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,25 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
-const prisma = new PrismaClient()
 
-prisma.user.create({
-  data: {
-    name: 'Fabrício',
-    email: 'fabriciolak12@gmail.com',
-  },
-})
+app.post('/users', async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
 
-app.get('/', async (request, reply) => {
-  reply.send('oi')
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
 })
