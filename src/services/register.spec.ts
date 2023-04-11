83ef66144c3775ec6154a015022919676762d8a6
Fabrício <fabriciolak12@gmail.com>
2023-04-11 09:40:30 -0300

    test: add unit test to register service

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -1,5 +1,33 @@
-import { expect, test } from 'vitest'
+import { it, describe } from 'vitest'
+import { RegisterUserCase } from './register'
+import { compare } from 'bcryptjs'
 
-test('check', () => {
-  expect(2).toBe(2)
+describe('Register use case', () => {
+  it('Must be hash password upon user register', async () => {
+    const registerUseCase = new RegisterUserCase({
+      async findByEmail(_) {
+        return null
+      },
+
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+    })
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const isPasswordHashed = await compare('123456', user.password_hash)
+
+    console.log(isPasswordHashed)
+  })
 })
