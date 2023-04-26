84a9986a0443e5c08e1208ccc55715a205fb4e90
Fabrício <fabriciolak12@gmail.com>
2023-04-26 15:37:19 -0300

    feat: creating profile controller

---
+++ src/http/controllers/profile.ts
@@ -0,0 +1,17 @@
+import { makeGetUsersProfileUseCase } from '@/services/factories/make-get-users-profile-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function profile(request: FastifyRequest, reply: FastifyReply) {
+  const getUserProfile = makeGetUsersProfileUseCase()
+
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
+
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
+}
