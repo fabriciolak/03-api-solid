033c4f376cd3d4f6b43a3ebc5912a7f335cce345
Fabrício <fabriciolak12@gmail.com>
2023-05-11 09:52:41 -0300

    test: add test - Added test to authenticate controller - Added test to profile controller - Added test to register controller

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,21 @@
-import { test } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
 
-test('oi', () => {})
+beforeAll(async () => {
+  await app.ready()
+})
+
+afterAll(async () => {
+  await app.close()
+})
+
+describe('Register (E2E)', () => {
+  it('Should be able register', async () => {
+    const response = await request(app.server)
+      .post('/users')
+      .send({ name: 'John Doe', email: 'johndoe@does.com', password: '123456' })
+
+    expect(response.statusCode).toBe(201)
+  })
+})
