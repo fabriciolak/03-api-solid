23f199fe05b9c043a4a05569f430007dda68243b
Fabrício <fabriciolak12@gmail.com>
2023-05-12 11:30:21 -0300

    refactor(services): change factorie function name

--- src/services/factories/make-get-users-metrics-use-case.ts
+++ src/services/factories/make-get-users-metrics-use-case.ts
@@ -1,7 +1,7 @@
 import { GetUserMetricsService } from '../get-users-metrics'
 import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository'
 
-export function makeGetUsersProfileUseCase() {
+export function makeGetUsersMetricsUseCase() {
   const checkInsRepository = new PrismaCheckInsRepository()
   const useCase = new GetUserMetricsService(checkInsRepository)
 
